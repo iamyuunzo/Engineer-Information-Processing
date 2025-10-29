51d39a9314ddd07905ba3cb739d2f3cec8fe81c4
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-10-29 22:44:22 +0900

    Review: 1029-java-wrong-anwser

---
+++ Java/ggr026.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        int[] a = new int[8];
+        int i = 0;
+        int n = 10;
+        while () {
+            a [i++] = ();
+            n /= 2;
+        }
+
+        for(i=7; i>=0; i--) {
+            System.out.print(a[i]);
+        }
+    }
+}
