701eef6d76637c26bed589a808c7a138d88eb072
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-10-31 22:30:17 +0900

    Review: 1031-java-wrong-answer

--- Java/ggr023.java
+++ Java/ggr023.java
@@ -1,3 +1,5 @@
+// 자바 코드를 실행할 경우 에러가 발생됨. 에러가 발생하는 라인은?
+
 class Person {
     private String name;
 
@@ -21,3 +23,7 @@ class Main {
     }
 }
 
+// 답 : 11번 라인 (return name)
+// 이유 : static 메소드에서는 static 변수만 사용 가능하고 멤버 변수 사용이 불가능함.
+// 해결 방법 - get()을 인스턴스 메서드로 바꾸기
+//          - name을 static 변수로 바꾸기
