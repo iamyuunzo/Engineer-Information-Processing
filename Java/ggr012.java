291adddd23eb81ad621dc3d4ae7ffef0f9e171e5
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-11-01 22:28:13 +0900

    Review: 1101-java-wrong-answer

--- Java/ggr012.java
+++ Java/ggr012.java
@@ -32,3 +32,12 @@ class Main {
         System.out.println(obj.getX());
     }
 }
+
+
+// Solving
+// 답 : 5000
+
+// 이유 : Child obj = new Child();에서 Child() 생성자가 호출된다.
+// Child() 생성자에서는 this(5000);이 호출되어 Child(int x) 생성자가 호출된다.
+// Child(int x) 생성자에서는 this.x = x;가 실행되어 Child 클래스의 멤버 변수 x가 5000으로 초기화된다.
+// 따라서 obj.getX()를 호출하면 5000이 출력된다.
--- Java/ggr017.java
+++ Java/ggr017.java
@@ -32,3 +32,18 @@ class Main {
         System.out.print(obj.getName());
     }
 }
+
+// Solving (어려우니까 꼭 다시 보기..)
+// 답 : Vehicle name: Spark
+
+// 코드 순서
+// 1. Vehicle obj = new Car("Spark"); 에서 Car 클래스의 생성자가 호출된다.
+// 2. Car 클래스의 생성자에서는 setName(val);이 호출되어 Vehicle 클래스의 setName 메서드가 실행된다.
+// 3. setName 메서드에서는 private 멤버 변수 name에 "Spark"가 할당된다.
+// 4. System.out.print(obj.getName());에서 Vehicle 클래스의 getName() 메서드가 호출된다.
+// 5. getName() 메서드에서는 "Vehicle name:" + name;이 반환되며, name에는 "Spark"가 할당되어 있으므로 "Vehicle name: Spark"가 출력된다.
+
+// obj.getName()에서 Car 클래스가 아니라 Vehicle 클래스의 getName() 메서드가 호출되는 이유는
+// obj의 타입이 Vehicle이기 때문이다. (getName()이 아니라 getName(String val)이지, 괄호가 빈 getName()은 만들지 않았기 때문)
+// 오버로딩된 getName 메서드는 매개변수의 타입과 개수에 따라 호출되는 메서드가 결정되는데,
+// obj.getName()은 매개변수가 없으므로 Vehicle 클래스의 getName() 메서드가 호출된다.
